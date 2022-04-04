15c31f06f9043a0c464aaff1b47eba6b4a718733
dotpegaso <dotpegaso@gmail.com>
2022-04-04 03:45:06 -0300

    upadte

--- index.js
+++ index.js
@@ -75,10 +75,14 @@ client.on("guildMemberRemove", async (member) => {
 });
 
 client.on("message", (message) => {
-  if (message.content.startsWith("!nick")) {
-    message.author.setNickname({
-      nick: "Souls of Mischief",
-    });
+  if (message.content.includes("atualizar_apelido")) {
+    if (!message.guild.me.hasPermission("MANAGE_NICKNAMES"))
+      return message.channel.send(
+        "I don't have permission to change your nickname!"
+      );
+    message.member.setNickname(
+      message.content.replace("atualizar_apelido ", "")
+    );
   }
 });
 
